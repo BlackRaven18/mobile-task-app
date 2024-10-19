6e160ca0165447027fe60f4595ed66ec58e9f609
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-10-19 16:25:11 +0200

    Removed unused files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
+import { ThemeProvider, DarkTheme, DefaultTheme } from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerTitle: "Task App" }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </ThemeProvider>
   );
 }
