7a74dc66a34441a5feddd7455114d6f89e0e2b9f
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-10-27 10:31:45 +0100

    Added add task screen

---
+++ task-app/app/add-task.tsx
@@ -0,0 +1,47 @@
+import HttpClient from "@/api/HttpClient";
+import { useState } from "react";
+import { View } from "react-native";
+import { Button, TextInput, Text } from "react-native-paper";
+import { router } from 'expo-router';
+
+export default function AddTaskScreen() {
+
+    const [taskDescription, setTaskDescription] = useState('');
+    const [message, setMessage] = useState('');
+
+    const httpClient = new HttpClient();
+
+    const addNewTask = (taskDescription: string) => {
+        
+        if (taskDescription === '') {
+            setMessage('Task description cannot be empty');
+            return;
+        }
+
+        httpClient.addTask(taskDescription)
+            .then((response) => {
+                console.log(response);
+                router.back();
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    return (
+        <View style={{ padding: 10 }}>
+            <TextInput
+                label="Task description"
+                value={taskDescription}
+                onChangeText={text => setTaskDescription(text)}
+            />
+
+            <Button mode="contained" onPress={() => addNewTask(taskDescription)}>
+                Add task
+            </Button>
+            <Text>
+                {message}
+            </Text>
+        </View>
+    );
+}
