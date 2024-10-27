62048206e8ec192cbcb041c3f695efb0dce459aa
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-10-27 10:30:39 +0100

    Added HttpClient

---
+++ task-app/api/HttpClient.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+export default class HttpClient {
+
+    readonly SERVER_URL = "http://192.168.4.164:3000";
+    
+    async getTasks(): Promise<Task[]> {
+        return axios.get(`${this.SERVER_URL}/tasks`)
+            .then((response) => {
+                return response.data
+            })
+            .catch((error) => {
+                console.error(error);
+                return [];
+            });
+    }
+
+    async addTask(description: string): Promise<String> {
+        return axios.post(`${this.SERVER_URL}/tasks`, { description })
+            .then((response) => {
+                return response.data;
+            })
+            .catch((error) => {
+                console.error(error);
+                return "";
+            });
+    }
+
+    async deleteTask(id: number): Promise<String> {
+        return axios.delete(`${this.SERVER_URL}/tasks/${id}`)
+            .then((response) => {
+                return response.data;
+            })
+            .catch((error) => {
+                console.error(error);
+                return "";
+            });
+    }
+
+    async updateTask(id: number, description: string): Promise<String> {
+        return axios.put(`${this.SERVER_URL}/tasks/${id}`, { description })
+            .then((response) => {
+                return response.data;
+            })
+            .catch((error) => {
+                console.error(error);
+                return "";
+            });
+    }
+}
