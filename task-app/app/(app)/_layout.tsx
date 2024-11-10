620f48d350b3102d3bc5d507cb96c35a1ef20cc8
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-11-10 15:29:41 +0100

    Fixed /refresh endpoint

--- task-app/app/(app)/_layout.tsx
+++ task-app/app/(app)/_layout.tsx
@@ -1,30 +1,28 @@
 import { Redirect, Stack } from "expo-router";
-import { Text } from "react-native-paper";
-
-import { useSession } from "@/auth/ctx";
 import { useAuth } from "@/auth/AuthContext";
+import { Appbar } from "react-native-paper";
+import CustomDrawer from "@/components/CustomDrawer";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function AppLayout() {
   const { isSignedIn } = useAuth();
-  // const { session, isLoading } = useSession();
 
-  // // You can keep the splash screen open, or render a loading screen like we do here.
-  // if (isLoading) {
-  //   return <Text>Loading...</Text>;
-  // }
-
-  // // Only require authentication within the (app) group's layout as users
-  // // need to be able to access the (auth) group and sign in again.
   if (!isSignedIn) {
-    // On web, static rendering will stop here as the user is not authenticated
-    // in the headless Node process that the pages are rendered in.
     return <Redirect href="/sign-in" />;
   }
 
   // This layout can be deferred because it's not the root layout.
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{headerTitle: 'Task App'}}/>
-    </Stack>
+    <>
+      <Appbar.Header>
+        <Appbar.Content title="Task App" />
+        <Appbar.Action icon="logout" onPress={() => { }} />
+        <Appbar.Action icon="account" onPress={() => { }} />
+        <Appbar.Action icon="menu" onPress={() => { }} />
+      </Appbar.Header>
+      <Stack>
+        <Stack.Screen name="index" options={{headerShown: false}} />
+      </Stack>
+    </>
   )
 }
