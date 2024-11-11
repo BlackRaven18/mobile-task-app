894afa32e9021b952fb9cf98614b0c549a7d8827
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-11-11 12:56:56 +0100

    Added task lists screen

--- task-app/app/_layout.tsx
+++ task-app/app/_layout.tsx
@@ -3,12 +3,17 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
 
+
+export const unstable_settings = {
+  initialRouteName: 'sign-in',
+}
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
     <AuthProvider>
       <PaperProvider>
-        <Stack screenOptions={{ headerShown: false }}>
+        <Stack screenOptions={{ headerShown: false,}}>
           <Stack.Screen name="sign-in" options={{ headerShown: true, headerTitle: 'Sign In' }} />
           <Stack.Screen name="sign-up" options={{ headerShown: true, headerTitle: 'Sign Up' }} />
         </Stack>
