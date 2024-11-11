c7e83988511dae7636197fa37e3ae3f34c70584e
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-11-11 16:18:01 +0100

    Added modal for adding task lists

--- backend/routes/taskLists.js
+++ backend/routes/taskLists.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { verifyToken } = require('../middleware/authMiddleware');
 const db = require('../database');
 
+// GET all username task lists
 router.get('/',verifyToken, (req, res) => {
 
     const { username } = req.query
@@ -23,5 +24,21 @@ router.get('/',verifyToken, (req, res) => {
         res.status(200).send(rows)
     })
 })
+
+// ADD task list
+router.post('/', verifyToken, (req, res) => {
+
+    const { title, username } = req.body
+
+    const stmt = db.prepare("INSERT INTO task_list (title, user_id) VALUES (?, (SELECT id FROM user WHERE username = ?))");
+    stmt.run(title, username, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        res.status(201).send("Task list created")
+    })
+})
+
+
 
 module.exports = router;
