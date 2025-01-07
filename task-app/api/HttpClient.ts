e19a1a445013f9aecc23aeda7a484bdc74afa86f
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2025-01-07 20:17:55 +0100

    Updated sign in screen

--- task-app/api/HttpClient.ts
+++ task-app/api/HttpClient.ts
@@ -6,7 +6,7 @@ import * as SecureStore from 'expo-secure-store';
 
 
 const api = axios.create({
-    baseURL: "http://192.168.1.7:3000",
+    baseURL: "http://192.168.1.3:3000",
     //baseURL: "http://localhost:3000",
 })
 
